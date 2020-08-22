d8c6b010eb2d5bd4094d49de79397728a26db319
Dares <61025226+ishantda@users.noreply.github.com>
2020-08-22 20:52:56 +0530

    Add files via upload

---
+++ src/main/java/com/capgemini/pecunia/bank/PecuniaBankIntegratedApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.pecunia.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuniaBankIntegratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecuniaBankIntegratedApplication.class, args);
+	}
+
+}
